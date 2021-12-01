523e18af53093122c18987fdb0fc4e991d8261c3
RRGayfutdinov <Ghost546@yandex.ru>
2021-12-01 15:44:06 +0300

    v0.2

--- app/src/main/java/com/example/scheduleapp/MainActivity.kt
+++ app/src/main/java/com/example/scheduleapp/MainActivity.kt
@@ -2,10 +2,47 @@ package com.example.scheduleapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.scheduleapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.bottom_nav_view)
+        val navController = findNavController(R.id.my_nav_host)
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.scheduleFragment,
+            R.id.menuFragment
+        ))
+        val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener{ menuItem ->
+            when (menuItem.itemId) {
+                R.id.scheduleFragment -> {
+                    val fragment = ScheduleFragment()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
+                        .commit()
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.menuFragment -> {
+                    val fragment = MenuFragment()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
+                        .commit()
+                    return@OnNavigationItemSelectedListener true
+
+                }
+                else ->return@OnNavigationItemSelectedListener false
+            }
+        }
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
+
